916882c44cbd584b91804532fa04cff08c5251d4
George Shopov <george.shopov@citrix.com>
2016-02-28 16:32:48 -0800

    MCVE to demonstrate the issue

---
+++ ClassLibrary1/ClassLibrary1.cpp
@@ -0,0 +1,20 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "ClassLibrary1.h"
+
+using namespace ClassLibrary1;
+
+Class1::Class1() {
+}
+
+Class1::~Class1() {
+    this->!Class1();
+}
+
+Class1::!Class1() {
+}
+
+void Class1::HelloWorld() {
+}
---
+++ ClassLibrary1/ClassLibrary1.h
@@ -0,0 +1,18 @@
+// ClassLibrary1.h
+
+#pragma once
+
+using namespace System;
+
+namespace ClassLibrary1 {
+
+    public ref class Class1
+    {
+        // TODO: Add your methods for this class here.
+    public:
+        Class1();
+        ~Class1();   // Dispose()
+        !Class1();   // Finalize()
+        void HelloWorld();
+    };
+}
